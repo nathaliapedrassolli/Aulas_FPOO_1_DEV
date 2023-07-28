529a8c459f2896f27718f212d0963805a40c4223
Nathália Pedrassolli <nathalia.pedreira@aluno.senai.br>
2023-07-28 10:58:14 -0300

    Declarando String como objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,11 +14,14 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Declaração de Variáveis
+        
+        // Declaração de Variáveis de tipos primitivos
         String nome = "Nathália";
         int idade = 16;
         boolean estudando = true;
         double media = 5.5;
+        
+        String bairro = new String("Centro");
         
     }
     
