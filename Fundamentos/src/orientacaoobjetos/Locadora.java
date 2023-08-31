b3fcdec555381533498396b32d68275eaa316967
Nathália Pedrassolli <nathalia.pedreira@aluno.senai.br>
2023-08-31 09:22:31 -0300

    Utilizando sobrecarga de métodos e construtores

---
+++ Fundamentos/src/orientacaoobjetos/Locadora.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package orientacaoobjetos;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Locadora {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        
+        Cliente c1 = new Cliente("Nathália Pedrassolli Pedreira");
+        System.out.println(c1);
+        
+        double valorLocacaoSimples = c1.calcularLocacao(3);
+        System.out.println("O valor para a locação do filme simples é " + 
+                valorLocacaoSimples);
+        
+    
+        
+        Cliente c2 = new Cliente("Maria Clara Tremocoldi Oliveira", 
+                "mariatremocoli@gmail.com");
+        System.out.println(c2);
+        
+                double valorLocacaoLancamento = c2.calcularLocacao(3);
+        System.out.println("O valor para a locação do filme lançamento é " + 
+                valorLocacaoLancamento);
+        
+    }
+    
+}
